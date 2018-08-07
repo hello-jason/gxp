2386805b5014f4d2aca09e3510a2f195d2fcb621
Jason Cross <code@hellojason.net>
2018-08-07 14:44:56 -0500

    chore(qjoypad): autoload on startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-qjoypad "GXP"
-./cycle-website.sh
+function launch_qjoypad () {
+  qjoypad "GXP"
+}
+
+function launch_game () {
+  /bin/bash /home/pi/gxp/cycle-website.sh
+}
+
+launch_qjoypad
+launch_game
 
